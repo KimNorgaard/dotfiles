bd5fdf7f341536fd735a8d23591aa498a41041ac
Kim Nørgaard <kn@netic.dk>
2014-03-10 09:28:26 +0100

    added shell

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 cd
-rm -rf .vim .vimrc .zshrc .tmux.conf
+
+rm -rf .vim .vimrc .zshrc .tmux.conf .shell
 
 ln -s .dotfiles/vimrc .vimrc
 ln -s .dotfiles/vim .vim
 ln -s .dotfiles/tmux.conf .tmux.conf
 ln -s .dotfiles/zshrc .zshrc
+ln -s .dotfiles/shell .shell
