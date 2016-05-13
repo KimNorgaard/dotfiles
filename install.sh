76fc312b30a34a66b5db9d883d6fa47bbd14c7ea
Kim Nørgaard <kn@netic.dk>
2016-05-13 12:18:14 +0200

    fix stuff

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ ln -s .dotfiles/tmux.conf .tmux.conf
 ln -s .dotfiles/zshrc .zshrc
 ln -s .dotfiles/zshenv .zshenv
 ln -s .dotfiles/shell .shell
+
+mkdir -p .oh-my-ssh/custom/themes
+cp .shell/zsh-themes/dracula.zsh-theme .oh-my-ssh/custom/themes/
