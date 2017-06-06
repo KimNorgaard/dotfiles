559f0987a0e7d0bbd653172affdb03e9b7a19ab4
Kim Nørgaard <jasen@jasen.dk>
2017-06-06 12:58:36 +0200

    update colors

--- shell/prompt.zsh
+++ shell/prompt.zsh
@@ -11,9 +11,9 @@ function parse_git_dirty() {
   fi
   STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
   if [[ -n $STATUS ]]; then
-    echo ") %{$fg_bold[yellow]%}✗ "
+    echo ") %{$fg[yellow]%}✗ "
   else
-    echo ") %{$fg_bold[green]%}✔ "
+    echo ") %{$fg[green]%}✔ "
   fi
 }
 
@@ -21,10 +21,12 @@ function git_prompt_info() {
   local ref
   ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-  echo "%{$fg_bold[cyan]%}(${ref#refs/heads/}$(parse_git_dirty)%{$reset_color%}"
+  echo "%{$fg[cyan]%}(${ref#refs/heads/}$(parse_git_dirty)%{$reset_color%}"
 }
 
 local ret_status="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+local ret_cwd="%(?:%{$fg[green]%}:%{$fg[red]%})%~"
 
-PROMPT='${ret_status} %{$fg_bold[blue]%}%c $(git_prompt_info)% %{$reset_color%}'
+#PROMPT='${ret_status} %{$fg_bold[blue]%}%c $(git_prompt_info)% %{$reset_color%}'
+PROMPT='%m:${ret_cwd} $(git_prompt_info)% %{$reset_color%}'
 
