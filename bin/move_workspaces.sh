c8975795fdd3f9cf788fdbfb4a5c9ebb8832bb24
Kim Nørgaard <jasen@jasen.dk>
2018-07-05 10:31:27 +0200

    new monitor placement

--- bin/move_workspaces.sh
+++ bin/move_workspaces.sh
@@ -3,13 +3,12 @@
 export DISPLAY=:0
 export XAUTHORITY=/home/kn/.Xauthority
 
-WS=${1:-eDP1}
-CURRENT_WS="$(i3-msg -t get_workspaces|jq -r '.[] | select(.focused==true).name')"
+#CURRENT_WS="$(i3-msg -t get_workspaces|jq -r '.[] | select(.focused==true).name')"
 
-i3-msg "workspace 1; move workspace to $WS"
-i3-msg "workspace 2; move workspace to $WS"
-i3-msg "workspace 3; move workspace to $WS"
-i3-msg "workspace 4; move workspace to $WS"
-i3-msg "workspace 5; move workspace to $WS"
+i3-msg "workspace 1:web; move workspace to output primary"
+i3-msg "workspace 2:mail; move workspace to output primary"
+i3-msg "workspace 3:term; move workspace to output primary"
+i3-msg "workspace 4:chat; move workspace to output primary"
+i3-msg "workspace 5; move workspace to output primary"
 
 i3-msg "workspace $CURRENT_WS"
