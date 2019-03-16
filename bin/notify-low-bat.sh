3b3e97170ac6daa6b8b3a993afc201cbb42c3519
Kim Nørgaard <jasen@jasen.dk>
2019-03-16 15:25:01 +0100

    update low power script to play nice with dbus

--- bin/notify-low-bat.sh
+++ bin/notify-low-bat.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-su -c "/usr/bin/dunstify -i battery-caution -r 4243 -a power -u critical \"BATTERY LOW<br>$POWER_SUPPLY_CAPACITY%\"" kn
+DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+su -p -c "/usr/bin/dunstify -i battery-caution -r 4243 -a power -u critical \"BATTERY LOW<br>$POWER_SUPPLY_CAPACITY%\"" kn
